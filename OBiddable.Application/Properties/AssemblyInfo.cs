401640b84b360d19485ecf6b1660d0fb46df3a61
Tiptup300 <Tiptup300@gmail.com>
2025-03-25 19:43:47 -0400

    work

--- OBiddable.Application/Properties/AssemblyInfo.cs
+++ OBiddable.Application/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CCD Bid Manager")]
+[assembly: AssemblyTitle("OBiddable")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Greater Altoona Career & Technology Center Computer Consortium")]
-[assembly: AssemblyProduct("ccd.bm.win")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OBiddable")]
+[assembly: AssemblyCopyright("Copyright ©2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5965c65c-4bea-4200-9165-d4629ae25d60")]
+[assembly: Guid("5965c65c-4bea-4200-9165-d4629ae25d61")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ README.md
@@ -0,0 +1,3 @@
+1. step one is convert to .net 8
+2. fix dependency package references
+3. get running
